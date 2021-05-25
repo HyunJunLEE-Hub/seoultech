d8b3d4334c2234b6433fc6a7adb873cb0e1ddf1c
leehyunjun-seoultech <80024878+leehyunjun-seoultech@users.noreply.github.com>
2021-05-25 23:51:06 +0900

    Update AutoConnect.py

--- AutoStockTrade/AutoConnect.py
+++ AutoStockTrade/AutoConnect.py
@@ -14,3 +14,11 @@ time.sleep(5)
 app = application.Application()
 app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id:아이디를 적으세요 /pwd:비밀번호를 적으세요 /pwdcert:공인인증서 비밀번호를 적으세요 /autostart') # 크레온 로그인 아이디 / 비밀번호 /공인인증서 비밀번호
 time.sleep(60)
+
+# 동적으로 받기
+
+#id = input("ID를 입력해주세요 : ")
+#pw = input("비밀번호를 입력해주세요 :")
+#pw_pw = input("공인인증서 비밀번호를 입력해주세요 : ")
+
+#app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id: {0} /pwd: {1} /pwdcert: {2} /autostart'.format(id,pw,pw_pw)) # 크레온 로그인 아이디 / 비밀번호 /공인인증서 비밀번호
