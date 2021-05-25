7bc9e9944371fdf4fb7c5c0abc1378d9246e987a
leehyunjun-seoultech <80024878+leehyunjun-seoultech@users.noreply.github.com>
2021-05-25 23:35:49 +0900

    Update AutoConnect.py

--- AutoStockTrade/AutoConnect.py
+++ AutoStockTrade/AutoConnect.py
@@ -12,5 +12,5 @@ os.system('wmic process where "name like \'%DibServer%\'" call terminate')
 time.sleep(5)        
 
 app = application.Application()
-app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id:userid /pwd:pa$$word /pwdcert:certPa$$word /autostart') # 크레온 로그인 아이디 / 비밀번호 /공인인증서 비밀번호
+app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id:아이디를 적으세요 /pwd:비밀번호를 적으세요 /pwdcert:공인인증서 비밀번호를 적으세요 /autostart') # 크레온 로그인 아이디 / 비밀번호 /공인인증서 비밀번호
 time.sleep(60)
